df659d53b128251786fd027109ad2f57acc46e78
shiatomaro <trishiagavile0911@mgmail.com>
2025-01-02 13:15:42 +0800

    added gohappy

---
+++ go.mod
@@ -0,0 +1,12 @@
+module try_discord
+
+go 1.23.4
+
+require (
+	github.com/bwmarrin/discordgo v0.28.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/exp v0.0.0-20241217172543-b2144cdd0a67 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
--- main.go
+++ main.go
@@ -1,50 +1,98 @@
 package main
 
-import {
-	"crypto/ed25519"
-	"crypto/subtle"
-	"encoding/json"
-	
-	"errors"
+import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"log"
 	"os"
-}
+	"os/signal"
+	"strings"
+	"syscall"
 
-//  Interaction represents the structure of an interaction received from Discord.
-type Interaction struct { 
-	Type int `json:"type"`
-}
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"golang.org/x/exp/rand"
+)
 
-// InteractionResponse represents the structure of the response sent to Discord.
-type InteractionResponse struct {
-	Type int `json:"type"`
-}
+const prefix string = "!gotrybot"
 
-const {
-	InteractionTypePing = 1  // Ping type interaction.
-	InteractionTypeCommand = 2 // Command Type Interaction.
-	InteractionResponsePong = 1 // Pong respong for a Ping interaction.
-}
+func main() {
+	// get token
+	godotenv.Load()
 
-// main starts the http server to and listen for incoming request.
-
-func main () {
-	// set up the http server to route all request to the handlerquest..
-	http.HandleFunc("/". handlerquest)
-	// read the server port fromt the envirorment variable  or port default to 8080.
-	port := os.Getenv("Port")
-	if port == "" {
-		port = "8080"
+	// store environment variable for the token.
+	token := os.Getenv("DISCORD_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("No discord bot token provided. Set DISCORD_BOT_TOKEN environment")
+	}
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-fmt.Printf("Starting server on port s%.....\n", port)
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		log.Println("Message received: ", m.Content)
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 
-// start the http server 
-if err := http.ListenAndServe(":"+port, nil); err !=nil {
-	 fmt.Printf("error starting server: %v\n", err)
+		args := strings.Split(m.Content, " ")
+
+		if args[0] != prefix {
+			return
+		}
+
+		if args[1] == "Commands" {
+			s.ChannelMessageSend(m.ChannelID, "gohappy: jokes")
+		}
+		if args[1] == "gohappy" {
+			gohappy := []string{
+				"How do trees get on the Internet? They log in.",
+				"What do computers like to eat? Chips.",
+				"What do you call a space magician? A flying saucerer.",
+				"What is a computer’s first sign of old age? Loss of memory.",
+				"What does a baby computer call his father? Instead of Da-da, it says 'Da-ta.'",
+				"What is an astronaut’s favorite control on the computer keyboard? The space bar.",
+				"What happened when the computer fell on the floor? It slipped a disk.",
+				"How does a boy cell phone propose to his girlfriend? He gives her a ring, of course.",
+				"Why was there a bug in the computer? It was looking for a byte to eat.",
+				"What is a computer virus? A terminal illness.",
+				"How did the mouse get out of the Roman Cathedral? He clicked on an icon and opened a window.",
+				"What kind of doctor fixes broken websites? A URLologist.",
+				"Have you heard about the Disney virus? It makes everything on your computer go Goofy.",
+				"What happened when a dragon breathed on several Macintosh computers? He wound up with baked Apples!",
+				"Why did the chicken cross the Web? To get to the other site.",
+				"Why did the computer go to a doctor? It thought it had a terminal illness.",
+				"Knock-knock! Who’s there? I-M. I-M, who? I-M on the computer, and I can’t answer the door.",
+			}
+
+			selection := rand.Intn(len(gohappy))
+
+			author := discordgo.MessageEmbedAuthor{
+				Name: "Uknown",
+				URL:  "https://www.elon.edu/u/imagining/about/kidzone/jokes-laughs/",
+			}
+
+			embed := discordgo.MessageEmbed{
+				Title:  gohappy[selection],
+				Author: &author,
+			}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+		}
+	})
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatalf("Error in creating a discord bot session: %v", err)
+	}
+	defer sess.Close()
+
+	fmt.Println("Bot is currently online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+	log.Println("Bot shutting down..")
+
 }
-}
-
-
